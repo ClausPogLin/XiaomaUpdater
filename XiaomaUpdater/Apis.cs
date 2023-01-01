8346b9eff66b7bad2357b808561715df453fc8e2
hakanalmer <hakanalmer@HAKANALMER9817>
2023-01-01 12:27:51 +0800

    Add project files.

---
+++ XiaomaUpdater/Apis.cs
@@ -0,0 +1,61 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XiaomaUpdater
+{
+    public class Apis
+    {
+        public string GetSportsList()
+        {
+            var client = new RestClient("https://data-lb.tanmasports.com/v1/exercisewx/getStudentWxComboExerciseV2");
+            var request = new RestRequest { Method = Method.Post };
+            request.AddHeader("Accept", "*/*");
+            request.AddHeader("Accept-Language", "en-US,en;q=0.9");
+            request.AddHeader("Accept-Encoding", "gzip, deflate, br");
+            request.AddHeader("token", "d69fae14b84caa4c7dc281a6c43cb03c");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("appKey", "e67dc5bd45ff495c");
+            request.AddHeader("Referer", "https://servicewechat.com/wxfdc7472438f38f8b/39/page-frame.html");
+            request.AddHeader("sign", "AE932C1BD55AB551FD4DA7A36E20B462encodeutf8");
+            var body = @"{""childrenId"":1490403}";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            return response.Content ?? "";
+        }
+
+        public string UpdateSport(long comboid,long planid,long timesec,string videourl="")
+        {
+            var client = new RestClient("https://data-lb.tanmasports.com/v1/exercisewx/submitChildrenExercise");
+            var request = new RestRequest { Method = Method.Post };
+            request.AddHeader("token", "d69fae14b84caa4c7dc281a6c43cb03c");
+            request.AddHeader("Content-Type", "application/json");
+            var body = @"{" + "\n" +
+            @"    ""innerSchool"": ""1""," + "\n" +
+            @"    ""comboItemId"": "" "+comboid+@" ""," + "\n" +
+            @"    ""planId"": "" "+ planid + @" ""," + "\n" +
+            @"    ""comboId"": "" "+ comboid + @" ""," + "\n" +
+            @"    ""times"": "+ timesec + "," + "\n" +
+            @"    ""groupCount"": ""6""," + "\n" +
+            @"    ""childrenId"": ""1490403""," + "\n" +
+            @"    ""videoUrl"": "" "+videourl+@" ""," + "\n" +
+            @"    ""haveVideo"": 1," + "\n" +
+            @"    ""notVideoRemark"": """"," + "\n" +
+            @"    ""pics"": []" + "\n" +
+            @"}";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            return response.Content ?? "";
+        }
+
+        public void SendMessage(string title,string text)
+        {
+            var client = new RestClient("https://api.day.app/WHkMpzrC5mTnuagU48Cvnm/"+title+"/"+text+"?level=timeSensitive");
+            var request = new RestRequest { Method = Method.Post };
+            client.Execute(request);
+        }
+    }
+}
---
+++ XiaomaUpdater/Program.cs
@@ -0,0 +1,86 @@
+﻿using XiaomaUpdater.Models;
+
+namespace XiaomaUpdater
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region 打印标题
+            Console.WriteLine(" __  ___                               _   _           _       _            \r\n \\ \\/ (_) __ _  ___  _ __ ___   __ _  | | | |_ __   __| | __ _| |_ ___ _ __ \r\n  \\  /| |/ _` |/ _ \\| '_ ` _ \\ / _` | | | | | '_ \\ / _` |/ _` | __/ _ \\ '__|\r\n  /  \\| | (_| | (_) | | | | | | (_| | | |_| | |_) | (_| | (_| | ||  __/ |   \r\n /_/\\_\\_|\\__,_|\\___/|_| |_| |_|\\__,_|  \\___/| .__/ \\__,_|\\__,_|\\__\\___|_|   \r\n                                            |_|                             ");
+            Console.WriteLine("\nBy:Lzy\n");
+            #endregion
+
+            #region 初始化
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            ConsoleHelper c = new ConsoleHelper();
+            Apis a = new Apis();
+            c.Output("初始化完成", ConsoleColor.Blue);
+            #endregion
+
+            #region 项目列表
+            var getSportsList = Models.GetSportsList.FromJson(a.GetSportsList());
+            try
+            {
+                c.Output("Post到" + getSportsList.Response.List.Count + "个项目:", ConsoleColor.Green);
+                foreach (var sport in getSportsList.Response.List)
+                {
+                    c.Output("(type:" + sport.ItemOrientationName + ") " + sport.PlanName, ConsoleColor.White, 1);
+                    c.Output("PlanId:" + sport.PlanId + " comboId:" + sport.ComboId, ConsoleColor.DarkGray, 2);
+                    c.Output("标准:" + sport.PlayGifTime + "毫秒一组,共" + sport.PlanGroupNum + "组", ConsoleColor.DarkGray, 2);
+                    c.Output(sport.DetailPresentation, ConsoleColor.DarkGray, 2);
+                }
+            }
+            catch (Exception ex) 
+            {
+                c.Output("错误[" + getSportsList.Code + "]已抛出->"+getSportsList.Msg, ConsoleColor.Red);
+                c.Output(ex.Message, ConsoleColor.DarkGray,1);
+                a.SendMessage("错误[" + getSportsList.Code + "]已抛出->" + getSportsList.Msg, "获取项目列表时发生错误");
+                return;
+            }
+            #endregion
+
+            #region 完成项目
+            int errorcount = 0;
+            c.Output("所有项目将被标记为有视频，时间为每个项目建议的标准时间", ConsoleColor.Yellow);
+            c.Output("开始提交"+getSportsList.Response.List.Count+"个项目", ConsoleColor.Blue);
+            foreach (var sport in getSportsList.Response.List)
+            {
+                updateItem:
+                c.Output(@"正提交 """ + sport.PlanName + @"""", ConsoleColor.White, 1);
+                var updateSport = Models.UpdateSport.FromJson(a.UpdateSport(sport.ComboId, sport.PlanId, (sport.PlanGroupNum * sport.PlayGifTime)));
+                c.Output(@"提交 """ + sport.PlanName + @""":共"+sport.PlanGroupNum+"组,每组耗时"+sport.PlayGifTime+"ms.", ConsoleColor.Green, 1);
+                if(updateSport.Msg!="成功")
+                {
+                    errorcount++;
+                    c.Output(@"->" + updateSport.Msg, ConsoleColor.Yellow, 2);
+                    a.SendMessage("错误[" + updateSport.Code + "]已抛出->" + updateSport.Msg, @"提交项目"""+sport.PlanName+@"""时发生错误");
+                }
+                c.Output(@"->"+updateSport.Msg, ConsoleColor.Green, 2);
+                Thread.Sleep(2000);
+            }
+            #endregion
+            c.Output("已完成提交,共"+ getSportsList.Response.List.Count+"个项目，错误"+errorcount+"个", ConsoleColor.White);
+            a.SendMessage("已完成提交,共" + getSportsList.Response.List.Count + "个项目，错误" + errorcount + "个", DateTime.Now.ToString("s"));
+        }
+
+    }
+
+    public class ConsoleHelper
+    {
+        public void Output(string message, ConsoleColor color = ConsoleColor.White, int treeLevel = 0)
+        {
+            Console.ForegroundColor = color;
+            string treeLevelTab = "";
+            if (treeLevel > 0)
+            {
+                for (int i = 0; i < treeLevel; i++)
+                {
+                    treeLevelTab += "   ";
+                }
+            }
+
+            Console.WriteLine("[ " + DateTime.Now + " ] " + treeLevelTab + message );
+        }
+    }
+}
